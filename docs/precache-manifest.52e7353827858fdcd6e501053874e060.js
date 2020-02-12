ad8853970304be9a7f89094e1686feb3140e98b8
Jungeun Choi <jechoi29@uw.edu>
2020-02-12 10:16:50 -0800

    Add template portfolio

---
+++ docs/precache-manifest.52e7353827858fdcd6e501053874e060.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d79663e389d0aa0c0d581a1a8643959c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4cc9a8166c582bc937b5",
+    "url": "/portfolio/static/css/main.b28f0fb4.chunk.css"
+  },
+  {
+    "revision": "3f7c1abb021fc6586ab9",
+    "url": "/portfolio/static/js/2.9bc43008.chunk.js"
+  },
+  {
+    "revision": "4cc9a8166c582bc937b5",
+    "url": "/portfolio/static/js/main.7ac2f4b7.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "4317be933b8b615a4b7847d40093ae3f",
+    "url": "/portfolio/static/media/header.4317be93.jpg"
+  }
+]);
